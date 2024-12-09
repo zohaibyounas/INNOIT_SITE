c60ea055e263b65f1975afc771f3ad8399b8e4dd
zohaibyounas <zohaiby737@gmail.com>
2024-12-09 13:24:18 +0500

    updated

--- app/contact/page.js
+++ app/contact/page.js
@@ -20,13 +20,16 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://innoit.org/send-email", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://backend-innoit.vercel.app/send-email",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       const data = await response.json(); // Read the response from the server
 
