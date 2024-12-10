527ad901c0e730b46081867fff85bdd53b66bab3
zohaibyounas <zohaiby737@gmail.com>
2024-12-10 12:20:46 +0500

    commited

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -1,34 +1,46 @@
 import { createTransport } from "nodemailer";
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  // Set CORS headers
-  const headers = {
-    "Access-Control-Allow-Origin": "*", // Or use specific domains, not "*"
-    "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
-  };
-
+  console.log("Received POST request to /api/email");
   try {
     const { name, email, message } = await req.json();
 
     if (!name || !email || !message) {
       return new Response(
         JSON.stringify({ message: "All fields are required." }),
-        { status: 400, headers }
+        { status: 400 }
       );
     }
 
-    // ... rest of the code
+    const transporter = createTransport({
+      host: process.env.SMTP_HOST,
+      port: 465, // Use 465 for SSL or 587 for STARTTLS
+      secure: true,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
 
-    return new Response(
-      JSON.stringify({ message: "Email sent successfully." }),
-      { status: 200, headers }
+    const mailOptions = {
+      from: `"${name}" <${email}>`,
+      to: process.env.RECIPIENT_EMAIL,
+      subject: `New Message from ${name}`,
+      text: `You have received a new message:\n\nName: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return NextResponse.json(
+      { message: "Email sent successfully" },
+      { status: 200 }
     );
   } catch (error) {
-    console.error("Error:", error);
-    return new Response(JSON.stringify({ message: "Failed to send email." }), {
-      status: 500,
-      headers,
-    });
+    console.error("Failed to send email:", error);
+    return NextResponse.json(
+      { message: "Failed to send email" },
+      { status: 500 }
+    );
   }
 }
--- app/contact/page.js
+++ app/contact/page.js
@@ -20,16 +20,13 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch(
-        "https://innoit-site-updated.vercel.app/api/email",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        }
-      );
+      const response = await fetch("/api/email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
       const data = await response.json(); // Read the response from the server
 
