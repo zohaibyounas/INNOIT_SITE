a7578aafb16625ce3ddbe965d02314c7763da069
zohaibyounas <zohaiby737@gmail.com>
2024-12-10 10:20:41 +0500

    commited

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -14,7 +14,7 @@ export async function POST(req) {
     const transporter = createTransport({
       host: process.env.SMTP_HOST,
       port: 465, // Use 465 for SSL or 587 for STARTTLS
-      // secure: true,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
